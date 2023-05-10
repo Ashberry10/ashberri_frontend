5ef2e2e050f138ab1dd72e831161cd7d3f207723
Aman kumar <amankumar02740@gmail.com>
2023-05-10 08:11:40 +0530

    server delopy in other service cloud ready to deloy frontend

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -44,7 +44,11 @@ export const authOptions:NextAuthOptions = {
             //   }
             
                  const {email,password } = credentials as any;
-                 const res = await fetch('http://127.0.0.1:8000/api/user/login/',{
+                //  const res = await fetch('http://127.0.0.1:8000/api/user/login/',{
+                 const res = await fetch('http://ashberri-api.onrender.com/api/user/login/',{
+
+
+
                   method:"POST",
                   headers:{
                     "Content-Type":"application/json"
--- pages/api/authApi.ts
+++ pages/api/authApi.ts
@@ -128,7 +128,10 @@ export const authApi = createApi({
  // The base query to request data.
  // RTK Query ships with fetchBaseQuery, which is a lightweight fetch wrapper that automatically handles request headers and response parsing in a manner similar to common libraries like axios.
  baseQuery: fetchBaseQuery({
-  baseUrl: 'http://127.0.0.1:8000/api/user/',
+  // baseUrl: 'http://127.0.0.1:8000/api/user/',
+  baseUrl: 'https://ashberri-api.onrender.com/api/user/',
+
+  // https://ashberri-api.onrender.com/
   // baseUrl:'https://jsonplaceholder.typicode.com/'
  }),
 
