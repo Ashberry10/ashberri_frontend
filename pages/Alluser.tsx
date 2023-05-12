cf140f2e2479174d10afd4d1b6c25045b7cb90dc
Aman kumar <amankumar02740@gmail.com>
2023-05-12 21:03:39 +0530

    added some more ui

--- pages/Alluser.tsx
+++ pages/Alluser.tsx
@@ -53,13 +53,17 @@ export interface NewsArticle {
 }
 
 
-
+interface FriendProps {
+  name: string
+  avatarUrl: string
+  status: string
+}
 
   
   
 
 
-export default function Home() {
+export default function Home({ name, avatarUrl, status }: FriendProps) {
 
 
 const {data:session} = useSession();
@@ -68,50 +72,98 @@ const {data,isSuccess} =  useAllUserPredictQuery(token)
 
   return (
   
-           <div className="todos">
-       {data?.map((curElem:any) => (
-          <div key={curElem.id}>
-           
-            <div>
+//     <div className="grid grid-cols-3 gap-4">
+// {data?.map((curElem:any) => (
+//    <div key={curElem.id}>
+//     <li className="bg-white rounded-lg shadow-lg overflow-hidden">
+//      <div>
 
-             <Link href={`/${curElem.id}`}> {curElem.FriendName}={curElem.Compatiblity}</Link>     
-             
-       
+//       <Link href={`/${curElem.id}`}> {curElem.FriendName}={curElem.Compatiblity}</Link>     
+      
 
-            </div>
+
+//      </div>
+//      </li>
+//    </div>
+//  ))} 
+
+
+// </div>
+
+
+// );
+// }
+
+
+
+
+
+
+
+<>
+<h1  className="font-bold text-xl">Friends</h1>
+
+    <div className="grid grid-cols-3 gap-4">
+
+{data?.map((curElem:any) => (
+
+   <div key={curElem.id}>
+    
+     {/* <div>
+
+      <Link href={`/${curElem.id}`}> {curElem.FriendName}={curElem.Compatiblity}</Link>     
+      
+
+
+     </div> */}
+
+
+
+     <div className="bg-gray-100 ">
+ <main className=" mx-auto   ">
+   <div className="px-2 py-2 ">
+     <ul className="">
+     <li className="bg-white rounded-lg shadow-lg overflow-hidden">
+     <Link  href={`/${curElem.id}`}>
+       <div className="relative">
+         <img className="w-full h-64 object-cover" src={avatarUrl} alt={name} />
+    
+       </div>
+       <div className="px-4 py-3">
+         <h2 className="text-gray-800 font-bold text-lg">
+         {/* <Link  href={`/${curElem.id}`}> */}
           
-          </div>
-        ))} 
-
+          {curElem.FriendName}
+          {/* <div className="absolute bottom-0 right-0 bg-gray-800 text-white py-2 px-4 rounded-bl-lg"> */}
+         {/* </div> */}
+          {/* </Link> */}
+          {/* &nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{curElem.Compatiblity} */}
+      {curElem.Compatiblity}
+          </h2>
 
        </div>
+       </Link>
+     </li>
+     </ul>
+   </div>
+ </main>
+ </div>
 
 
-  );
+
+
+   
+   </div>
+ ))} 
+
+
+</div>
+
+</>
+);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      
 
 
 
