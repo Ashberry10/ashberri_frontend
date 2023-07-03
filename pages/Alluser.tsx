5ed774459cc0488384027dc9e9b051102cee4757
Aman kumar <amankumar02740@gmail.com>
2023-07-03 23:04:57 +0530

    fixed loading issue while sendfriedrequest and cancel request...

--- pages/Alluser.tsx
+++ pages/Alluser.tsx
@@ -1,5 +1,5 @@
-import { useSession } from "next-auth/react";
 import { useState } from "react";
+import { useSession } from "next-auth/react";
 import { useAllUserPredictQuery } from "./api/authApi";
 import {
   useSendFriendRequestMutation,
@@ -17,44 +17,62 @@ interface FriendProps {
 export default function AllUser({ name, avatarUrl, status }: FriendProps) {
   const { data: session } = useSession();
   const token: any = session?.user.accessToken;
-  const { data, isSuccess } = useAllUserPredictQuery(token);
-  const { data: friendStatuses } = useGetAllUserFriendStatusQuery(token);
-  const [sendFriendRequest, { isLoading: isSendingFriendRequest }] = useSendFriendRequestMutation();
-  const [cancelFriendRequest, { isLoading: isCancelingFriendRequest }] = useCancelFriendRequestMutation();
+  const { data: allUsersData, isSuccess: isAllUsersDataSuccess } = useAllUserPredictQuery(token);
+  const { data: friendStatusesData, refetch: refetchFriendStatuses } = useGetAllUserFriendStatusQuery(token);
+  const [sendingRequests, setSendingRequests] = useState<string[]>([]);
+  const [cancelingRequests, setCancelingRequests] = useState<string[]>([]);
+  const [sendFriendRequest] = useSendFriendRequestMutation();
+  const [cancelFriendRequest] = useCancelFriendRequestMutation();
+
+  let allUsers = allUsersData || [];
+  let friendStatuses = friendStatusesData || [];
 
   const handleSendFriendRequest = async (friendId: string) => {
     try {
+      setSendingRequests((prevRequests) => [...prevRequests, friendId]);
       await sendFriendRequest({ access: token, formData: { receiver: friendId } });
       console.log(`Friend request sent successfully to ${friendId}`);
+      refetchFriendStatuses(); // Fetch updated friend statuses
     } catch (error: any) {
       console.log(`Failed to send friend request to ${friendId}: ${error.message}`);
+    } finally {
+      setSendingRequests((prevRequests) => prevRequests.filter((id) => id !== friendId));
     }
   };
 
   const handleCancelFriendRequest = async (friendId: string) => {
     try {
-      await cancelFriendRequest({ access: token,formData: { receiver: friendId } });
+      setCancelingRequests((prevRequests) => [...prevRequests, friendId]);
+      await cancelFriendRequest({ access: token, formData: { receiver: friendId } });
       console.log(`Friend request canceled for ${friendId}`);
+      refetchFriendStatuses(); // Fetch updated friend statuses
     } catch (error: any) {
       console.log(`Failed to cancel friend request for ${friendId}: ${error.message}`);
+    } finally {
+      setCancelingRequests((prevRequests) => prevRequests.filter((id) => id !== friendId));
     }
   };
+
+  if (!isAllUsersDataSuccess) {
+    // Handle loading state or error state
+    return null;
+  }
 
   return (
     <>
       <h1 className="font-bold text-2xl mb-4">Friends</h1>
 
       <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-        {data?.map((curElem: any) => {
-          const friendStatus = friendStatuses?.find((friend: any) => friend.friend_id === curElem.id);
+        {allUsers.map((curElem: any) => {
+          const friendStatus = friendStatuses.find((friend: any) => friend.friend_id === curElem.id);
+          const isSendingFriendRequest = sendingRequests.includes(curElem.id);
+          const isCancelingFriendRequest = cancelingRequests.includes(curElem.id);
 
           return (
             <div key={curElem.id} className="bg-gray-100 rounded-lg shadow-lg overflow-hidden">
-              <Link href={`/${curElem.id}`}>
-                <div className="relative">
-                  <img className="w-full h-56 object-cover" src={curElem.avatarUrl} alt={curElem.name} />
-                </div>
-              </Link>
+              <div className="relative">
+                <img className="w-full h-56 object-cover" src={curElem.avatarUrl} alt={curElem.name} />
+              </div>
               <div className="px-4 py-3">
                 <h1 className="text-lg font-semibold">{curElem.name}</h1>
                 <p className="text-sm text-gray-500">{curElem.status}</p>
@@ -66,10 +84,10 @@ export default function AllUser({ name, avatarUrl, status }: FriendProps) {
                   <span className="text-sm font-medium mr-1">Compatibility:</span>
                   <span className="text-sm">{curElem.Compatibility}</span>
                 </div>
-                <div className="flex items-center mt-1">
+                {/* <div className="flex items-center mt-1">
                   <span className="text-sm font-medium mr-1">Status:</span>
                   <span className="text-sm">{friendStatus?.friend_status || 'Friend Request Not Sent'}</span>
-                </div>
+                </div> */}
                 {friendStatus?.friend_status === 'Pending' && (
                   <button
                     className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-3 w-full"
@@ -109,13 +127,15 @@ export default function AllUser({ name, avatarUrl, status }: FriendProps) {
 
 
 
+
 // import { useSession } from "next-auth/react";
 // import { useState } from "react";
-// import { useAllUserPredictQuery,} from "./api/authApi";
-// import { useSendFriendRequestMutation, useCancelFriendRequestMutation, useGetAllUserFriendStatusQuery  } from "./api/friendApi";
-// import { getSession } from 'next-auth/react';
-// import { getToken } from "next-auth/jwt";
-// import { useQueryClient } from 'react-query';
+// import { useAllUserPredictQuery } from "./api/authApi";
+// import {
+//   useSendFriendRequestMutation,
+//   useCancelFriendRequestMutation,
+//   useGetAllUserFriendStatusQuery,
+// } from "./api/friendApi";
 // import Link from "next/dist/client/link";
 
 // interface FriendProps {
@@ -124,30 +144,39 @@ export default function AllUser({ name, avatarUrl, status }: FriendProps) {
 //   status: string;
 // }
 
-// export default function Home({ name, avatarUrl, status }: FriendProps) {
+// export default function AllUser({ name, avatarUrl, status }: FriendProps) {
 //   const { data: session } = useSession();
 //   const token: any = session?.user.accessToken;
 //   const { data, isSuccess } = useAllUserPredictQuery(token);
-//   const { data: friendStatuses } = useGetAllUserFriendStatusQuery(token);
-//   const [sendFriendRequest, { isLoading: isSendingFriendRequest }] = useSendFriendRequestMutation();
-//   const [cancelFriendRequest, { isLoading: isCancelingFriendRequest }] = useCancelFriendRequestMutation();
-//   // const queryClient = useQueryClient();
+//   const { data: friendStatuses, refetch: refetchFriendStatuses } = useGetAllUserFriendStatusQuery(token);
+//   const [sendingRequests, setSendingRequests] = useState<string[]>([]);
+//   const [cancelingRequests, setCancelingRequests] = useState<string[]>([]);
+//   const [sendFriendRequest] = useSendFriendRequestMutation();
+//   const [cancelFriendRequest] = useCancelFriendRequestMutation();
 
 //   const handleSendFriendRequest = async (friendId: string) => {
 //     try {
+//       setSendingRequests((prevRequests) => [...prevRequests, friendId]);
 //       await sendFriendRequest({ access: token, formData: { receiver: friendId } });
 //       console.log(`Friend request sent successfully to ${friendId}`);
+//       refetchFriendStatuses(); // Fetch updated friend statuses
 //     } catch (error: any) {
 //       console.log(`Failed to send friend request to ${friendId}: ${error.message}`);
+//     } finally {
+//       setSendingRequests((prevRequests) => prevRequests.filter((id) => id !== friendId));
 //     }
 //   };
 
 //   const handleCancelFriendRequest = async (friendId: string) => {
 //     try {
-//       await cancelFriendRequest({ access: token, friendId: friendId });
+//       setCancelingRequests((prevRequests) => [...prevRequests, friendId]);
+//       await cancelFriendRequest({ access: token, formData: { receiver: friendId } });
 //       console.log(`Friend request canceled for ${friendId}`);
+//       refetchFriendStatuses(); // Fetch updated friend statuses
 //     } catch (error: any) {
 //       console.log(`Failed to cancel friend request for ${friendId}: ${error.message}`);
+//     } finally {
+//       setCancelingRequests((prevRequests) => prevRequests.filter((id) => id !== friendId));
 //     }
 //   };
 
@@ -158,14 +187,14 @@ export default function AllUser({ name, avatarUrl, status }: FriendProps) {
 //       <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
 //         {data?.map((curElem: any) => {
 //           const friendStatus = friendStatuses?.find((friend: any) => friend.friend_id === curElem.id);
+//           const isSendingFriendRequest = sendingRequests.includes(curElem.id);
+//           const isCancelingFriendRequest = cancelingRequests.includes(curElem.id);
 
 //           return (
 //             <div key={curElem.id} className="bg-gray-100 rounded-lg shadow-lg overflow-hidden">
-//               <Link href={`/${curElem.id}`}>
-//                 <div className="relative">
-//                   <img className="w-full h-56 object-cover" src={curElem.avatarUrl} alt={curElem.name} />
-//                 </div>
-//               </Link>
+//               <div className="relative">
+//                 <img className="w-full h-56 object-cover" src={curElem.avatarUrl} alt={curElem.name} />
+//               </div>
 //               <div className="px-4 py-3">
 //                 <h1 className="text-lg font-semibold">{curElem.name}</h1>
 //                 <p className="text-sm text-gray-500">{curElem.status}</p>
@@ -207,4 +236,10 @@ export default function AllUser({ name, avatarUrl, status }: FriendProps) {
 //     </>
 //   );
 // }
+
+
+
+
+
+
 
