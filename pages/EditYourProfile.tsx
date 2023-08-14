d57505b7fbc12283aafbc9ab373d0d518f1f9134
AMAN KUMAR <amankumar02740@gmail.com>
2023-08-14 15:05:52 +0530

    file upload fixed

--- pages/EditYourProfile.tsx
+++ pages/EditYourProfile.tsx
@@ -550,7 +550,7 @@ const EditYourProfile = () => {
                           <div className="text-sm">
                             <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                               <span>Select a file</span>
-                              {/*  */}
+                              
                               <input
                                 id="file-upload"
                                 name="file"
