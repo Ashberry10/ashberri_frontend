856f600e50bd61a80a540c1f7e0bc138f702fc61
Ashish Thakur <50015357+AshishThakur10@users.noreply.github.com>
2023-08-26 11:16:36 +0530

    Profile Image
    render profile Image of loged in user

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['223.235.84.152'], // IP address
+  },
 }
 
 module.exports = nextConfig
