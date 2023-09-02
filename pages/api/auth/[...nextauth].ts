45712493cde4d5900757ee16adc7c74093f88e55
Amankumar10 <amankumar02740@gmail.com>
2023-09-02 16:00:54 +0530

    bugs fixed - we are friend is not showing in sender profile

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -36,7 +36,7 @@ export const authOptions:NextAuthOptions = {
             //   }
             async authorize(credentials, req) {
                 const {email,password } = credentials as any;
-                // const res = await fetch('http://127.0.0.1:8000/account/login/', {
+                // const res = await fetch('http://127.0.0.1:8001/account/login/', {
                 // const res = await fetch('http://223.235.84.204:8000/account/login/', {
 
                 const res = await fetch('http://ashberri.in:8000/account/login/', {
