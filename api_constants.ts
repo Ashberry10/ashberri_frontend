af7668dc26df4744f3211171826146e66134c687
Amankumar10 <amankumar02740@gmail.com>
2023-12-14 12:43:15 +0530

    just checking something..

--- api_constants.ts
+++ api_constants.ts
@@ -1,9 +1,9 @@
 
 
 
-// export const BASE_URL: string = "http://localhost:8000"; 
+export const BASE_URL: string = "http://localhost:8000"; 
 
-export const BASE_URL: string = 'http://ashberri.in:8000';
+// export const BASE_URL: string = 'http://ashberri.in:8000';
 
 // export const BASE_URL: string = 'http://223.235.85.60:8000';
 
